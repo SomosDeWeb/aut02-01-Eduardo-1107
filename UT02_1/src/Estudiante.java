051056ec697eb51af1b8f2d1318d5697fe011152
Eduardo-1107 <eduardoarmas1107@gmail.com>
2025-10-04 12:55:43 +0100

    Añadidos Constructor y getter and setters en Estudiante

--- UT02_1/src/Estudiante.java
+++ UT02_1/src/Estudiante.java
@@ -1,3 +1,31 @@
 public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double notaMedia;
+    private boolean esMatriculado;
+
+    public Estudiante() { }
+
+     public String getNombre() {
+        return this.nombre;
+     }
+     public int getEdad() {
+        return this.edad;
+     }
+     public double getNotaMedia() {
+        return this.notaMedia;
+     }
+     public boolean getEsMatriculado() {
+        return this.esMatriculado;
+     }
+     public void setEdad(int edad) {
+        this.edad = edad;
+     }
+     public void setNotaMedia(double notaMedia) {
+        this.notaMedia = notaMedia;
+     }
+     public void setEsMatriculado(boolean esMatriculado) {
+        this.esMatriculado = esMatriculado;
+     }
 
 }
