6e7e6196539476cb3c56439a3de094a462a265d3
Eduardo-1107 <eduardoarmas1107@gmail.com>
2025-10-04 12:38:25 +0100

    Añadido clase Menu y clase Estudiante

--- UT02_1/src/Main.java
+++ UT02_1/src/Main.java
@@ -2,4 +2,18 @@ public class Main {
     static void main() {
 
     }
+    public class Menu {
+        public static void menu() {
+            System.out.println("""
+                    === Gestor de Estudiantes ===
+                        1. Añadir Estudiantes
+                        2. Listar Estudiantes
+                        3. Buscar por nombre
+                        4. Calcular nota media general
+                        5. Mostar mejor estudiante
+                        6. Salir
+                    """);
+        }
+    }
+
 }
