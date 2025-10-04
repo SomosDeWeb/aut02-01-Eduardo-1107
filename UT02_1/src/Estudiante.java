c548e5c6acb449ffdde0cc0f5b6578614ab0167f
Eduardo-1107 <eduardoarmas1107@gmail.com>
2025-10-04 13:13:38 +0100

    Mejorada clase Estudiante

--- UT02_1/src/Estudiante.java
+++ UT02_1/src/Estudiante.java
@@ -18,8 +18,14 @@ public class Estudiante {
      public boolean getEsMatriculado() {
         return this.esMatriculado;
      }
+     public void setNombre(String nombre) {
+        this.nombre = nombre;
+     }
      public void setEdad(int edad) {
-        this.edad = edad;
+        if (edad > 0)
+            this.edad = edad;
+        else
+            System.out.println("No se ha podido establecer la edad. Debe tener más de 0 años"); // No sé si debería poner un argumentException
      }
      public void setNotaMedia(double notaMedia) {
         this.notaMedia = notaMedia;
